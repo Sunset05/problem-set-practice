939b551f2002641de347d547da9df0088813b338
Keith Funk <Keith.C.Funk@gmail.com>
2021-09-16 11:55:53 -0600

    completed split string algorithm that with split a string of characters into an array with two characters each. implemented regex within solution

---
+++ codewars/split-string-into-2-character-elements/index.js
@@ -0,0 +1,15 @@
+/*breakdown 
+  first check for empty string, if so immediately return an empty array
+
+  then make sure that the string has an even number of characters, if it doesn't at an underscore on the end
+
+  regex time. use .match to look for any two characters globalle, and seperate them into their own element in the resulting array. return that value.
+*/
+function solution(str) {
+  if (str.length === 0) {
+    return [];
+  }
+  const forcedEvenString = str.length % 2 ? str + "_" : str;
+
+  return forcedEvenString.match(/../g);
+}
